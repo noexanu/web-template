cd7eaca3c16ba59e4acbbc338350d879c716dd44
Yan Salanovich <yan.salanovich@moneyboxapp.com>
2024-12-29 22:36:22 +0100

    init storybook

---
+++ libs/components/src/components/buttons/Button/Button.tsx
@@ -0,0 +1,13 @@
+import type { JSX } from "react";
+
+export type ButtonProps = JSX.IntrinsicElements["button"] & {
+  label: string;
+};
+
+export const Button = ({ label, ...props }: ButtonProps) => {
+  return (
+    <button type="button" {...props}>
+      {label}
+    </button>
+  );
+};
---
+++ libs/components/tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "tailwindcss";
+
+const tailwindConfig: Config = {
+  content: ["./src/**/*.{ts,tsx}"],
+  presets: [],
+};
+
+export default tailwindConfig;
