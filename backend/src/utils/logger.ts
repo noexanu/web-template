7d77a8a85f3d1dc1a687332e8621798087d66aa6
Yan Salanovich <70746953+noexanu@users.noreply.github.com>
2025-03-02 08:59:58 +0000

    improve error handling (#6)

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -1,8 +1,16 @@
 import pino from "pino";
 
 import { envConfig } from "../config/envConfig";
+import { REDACTION_CENSOR, REDACTION_PATHS } from "../const/logger.const";
 
 export const logger = pino({
   transport: { target: "pino-pretty" },
   level: envConfig.LOGGING_LEVEL,
+  serializers: {
+    req: ({ headers, url }) => ({ headers, url }),
+  },
+  redact: {
+    paths: REDACTION_PATHS,
+    censor: REDACTION_CENSOR,
+  },
 });
